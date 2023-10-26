fd53d617fb6d209c48ee2f69198b2f0eabd07b45
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-10-26 18:06:50 +0400

    Admin template uploaded and some controllers pushed

---
+++ Pustok/Pustok/Controllers/Admin/ProductController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.Database.DomainModels;
+using Pustok.Database.Repositories;
+using Pustok.ViewModels;
+
+namespace Pustok.Controllers.Admin;
+
+[Route("admin/products")]
+public class ProductController : Controller
+{
+    private readonly ProductRepository _productRepository;
+    private readonly CategoryRepository _categoryRepository;
+
+    public ProductController()
+    {
+        _productRepository = new ProductRepository();
+        _categoryRepository = new CategoryRepository();
+    }
+
+    #region Products
+
+    [HttpGet] //admin/products
+    public IActionResult Products()
+    {
+        return View("Views/Admin/Product/Products.cshtml", _productRepository.GetAllWithCategories());
+    }
+
+    #endregion
+
+    #region Add
+
+    [HttpGet("add")]
+    public IActionResult Add()
+    {
+        var categories = _categoryRepository.GetAll();
+        var model = new ProductAddResponseViewModel
+        {
+            Categories = categories
+        };
+
+        return View("Views/Admin/Product/ProductAdd.cshtml", model);
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(Product product)
+    {
+        _productRepository.Insert(product);
+
+        return RedirectToAction("Products");
+    }
+
+    #endregion
+
+    #region Edit
+
+    [HttpGet("edit")]
+    public IActionResult Edit(int id)
+    {
+        Product product = _productRepository.GetById(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        var model = new ProductUpdateResponseViewModel
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Rating = product.Rating,
+            Categories = _categoryRepository.GetAll(),
+            CategoryId = product.CategoryId
+        };
+
+        return View("Views/Admin/Product/ProductEdit.cshtml", model);
+    }
+
+    [HttpPost("edit")]
+    public IActionResult Edit(ProductUpdateRequestViewModel model)
+    {
+        Product product = _productRepository.GetById(model.Id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        product.Price = model.Price;
+        product.Rating = model.Rating;
+        product.CategoryId = model.CategoryId;
+
+        _productRepository.Update(product);
+
+        return RedirectToAction("Views/Admin/Product/ProductEdit.cshtml", "Products");
+    }
+
+    #endregion
+
+    #region Delete
+
+    [HttpGet("delete")]
+    public IActionResult Delete(int id)
+    {
+        Product product = _productRepository.GetById(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _productRepository.RemoveById(id);
+
+        return RedirectToAction("Products");
+    }
+
+    #endregion
+
+    protected override void Dispose(bool disposing)
+    {
+        _productRepository.Dispose();
+        _categoryRepository.Dispose();
+
+        base.Dispose(disposing);
+    }
+}
--- Pustok/Pustok/ViewModels/ProductAddResponseViewModel.cs
+++ Pustok/Pustok/ViewModels/ProductAddResponseViewModel.cs
@@ -5,5 +5,11 @@ namespace Pustok.ViewModels;
 
 public class ProductAddResponseViewModel
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Rating { get; set; }
+    public int? CategoryId { get; set; }
+
     public List<Category> Categories { get; set; }
 }
