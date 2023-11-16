b3baa213ef3d6a79cffa702cd7fa21174c663536
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-11-16 21:15:09 +0400

    Single product details endpoint completed

---
+++ Pustok/Pustok/Controllers/Client/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.Database;
+using System.Linq;
+
+namespace Pustok.Controllers.Client;
+
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult SingleProduct(int id, [FromServices] PustokDbContext pustokDbContext)
+    {
+        var product = pustokDbContext.Products
+            .Include(p => p.Category)
+            .Include(p => p.ProductSizes)
+                .ThenInclude(ps => ps.Size)
+            .Include(p => p.ProductColors)
+                .ThenInclude(pc => pc.Color)
+            .FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+}
