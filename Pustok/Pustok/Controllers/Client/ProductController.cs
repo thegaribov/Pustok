37b06895688f9483743671979abae97751681774
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-11-16 21:33:13 +0400

    Product details endpoint completed

--- Pustok/Pustok/Controllers/Client/ProductController.cs
+++ Pustok/Pustok/Controllers/Client/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pustok.Database;
+using Pustok.ViewModels.Product;
 using System.Linq;
 
 namespace Pustok.Controllers.Client;
@@ -28,5 +29,48 @@ public class ProductController : Controller
         }
 
         return View(product);
+    }
+
+    public IActionResult ProductDetails(int id, [FromServices] PustokDbContext pustokDbContext)
+    {
+        var product = pustokDbContext.Products
+            .Include(p => p.Category)
+            .Include(p => p.ProductSizes)
+                .ThenInclude(ps => ps.Size)
+            .Include(p => p.ProductColors)
+                .ThenInclude(pc => pc.Color)
+            .FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        var productViewModel = new ProductDetailsViewModel
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Rating = product.Rating,
+            CategoryName = product.Category.Name,
+            Colors = product.ProductColors
+                .Select(pc =>
+                    new ProductDetailsViewModel.ColorDetailsViewModel
+                    {
+                        Id = pc.ColorId,
+                        Name = pc.Color.Name
+                    })
+                .ToList(),
+            Sizes = product.ProductSizes
+                .Select(pc =>
+                    new ProductDetailsViewModel.SizeDetailsViewModel
+                    {
+                        Id = pc.SizeId,
+                        Name = pc.Size.Name
+                    })
+                .ToList()
+        };
+
+        return Json(productViewModel);
     }
 }
---
+++ Pustok/Pustok/ViewModels/Product/ProductDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Pustok.ViewModels.Product
+{
+    public class ProductDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+
+        public string CategoryName { get; set; }
+        public List<ColorDetailsViewModel> Colors { get; set; }
+        public List<SizeDetailsViewModel> Sizes { get; set; }
+
+
+
+        public class ColorDetailsViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class SizeDetailsViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
