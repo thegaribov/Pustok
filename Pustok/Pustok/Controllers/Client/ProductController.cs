685cfdfd980a7744b88f4804f1b7129b2081714b
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-11-20 21:05:33 +0400

    Partial view based product details modal functionality completed

--- Pustok/Pustok/Controllers/Client/ProductController.cs
+++ Pustok/Pustok/Controllers/Client/ProductController.cs
@@ -73,4 +73,47 @@ public class ProductController : Controller
 
         return Json(productViewModel);
     }
+
+    public IActionResult ProductDetailsModalView(int id, [FromServices] PustokDbContext pustokDbContext)
+    {
+        var product = pustokDbContext.Products
+            .Include(p => p.Category)
+            .Include(p => p.ProductSizes)
+                .ThenInclude(ps => ps.Size)
+            .Include(p => p.ProductColors)
+                .ThenInclude(pc => pc.Color)
+            .FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        var productViewModel = new ProductDetailsViewModel
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Rating = product.Rating,
+            CategoryName = product.Category.Name,
+            Colors = product.ProductColors
+                .Select(pc =>
+                    new ProductDetailsViewModel.ColorDetailsViewModel
+                    {
+                        Id = pc.ColorId,
+                        Name = pc.Color.Name
+                    })
+                .ToList(),
+            Sizes = product.ProductSizes
+                .Select(pc =>
+                    new ProductDetailsViewModel.SizeDetailsViewModel
+                    {
+                        Id = pc.SizeId,
+                        Name = pc.Size.Name
+                    })
+                .ToList()
+        };
+
+        return PartialView("Partials/Client/_ProductDetailsModalBodyPartialView", productViewModel);
+    }
 }
